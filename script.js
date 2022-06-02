1e1f74a93d89cecb15885ab88d65d1ad3668fdf9
Heitor Lira <lira.jooky@gmail.com>
2022-06-02 19:43:35 -0300

    Desenha letras corretamente & valida fim de jogo

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
    4. FIXME: eixo, desenharTracinho(), escreverLetraCorreta(); <-- Mudar o nome das funções e variáveis
 */
 
-//#region --- Variáveis
+//#region ---> Variáveis
 var palavras = ['ALURA', 'ORACLE', 'HTML', 'CSS', 'JAVASCRIPT']; // Array de palavras a serem sorteadas #TODO: adicionar mais
 var palavraSecreta = palavras[Math.floor(Math.random() * palavras.length)]; // Sorteia uma palavra e arredonda
 
@@ -14,6 +14,7 @@ var letrasErradas = []; // -> Array de letras erradas & não podem ser repetidas
 var letrasCorretas = []; // -> faz parte da palavra secreta
 
 var canvas = document.querySelector('#canvas').getContext('2d'); // -> tabuleiro do jogo | Onde será desenhado
+//#endregion ---> Variáveis
 
 // FIXME: Apagar LOG
 console.log(palavraSecreta);
@@ -24,14 +25,31 @@ document.addEventListener('keydown', (evento) => {
         var letra = evento.key.toUpperCase(); // evento.key -> pega a letra da tecla pressionada (não o código)
 
         // Essa letra é repetida? Sim -> mostrarAvisoLetraRepetida()
-        if (letrasErradas.includes(letra)) {
+        if (letrasErradas.includes(letra) || letrasCorretas.includes(letra)) {
             mostrarAvisoLetraRepetida(); // Evitar que a mesma letra seja usada mais de uma vez
         } else {
             // A letra tá na palavra secreta?
             if (palavraSecreta.includes(letra)) {
-                letrasCorretas.push(letra); // Sim -> adiciona letra na array de letras corretas
+                 // Sim -> adiciona letra na array de letras corretas
+                escreverLetraCorreta(palavraSecreta.indexOf(letra)); // Desenha a letra na posição correta
+                for (let i = 0; i < palavraSecreta.length; i++) {
+                    if (palavraSecreta[i] === letra) {
+                        escreverLetraCorreta(i); // Desenha a letra na posição correta
+                        letrasCorretas.push(letra);
+                    }
+                }
+                // Condição de vitória
+                if (letrasCorretas.length === palavraSecreta.length) {
+                    alert('Parabéns! Você ganhou!');
+                }
             } else {
                 letrasErradas.push(letra); // Não -> adiciona letra na array de letras erradas
+                vidas--; // Diminui uma vida
+                // Condição de derrota
+                if (vidas <= 0) {
+                    gameOver(); // Perdeu o jogo
+                    // location.reload(); // Recarrega a página
+                }
             }
         }
 
@@ -40,8 +58,9 @@ document.addEventListener('keydown', (evento) => {
 
     //FIXME: Apagar LOGs
     console.log('Letra inserida: ' + letra);
-    console.log('Erradas ->' + letrasErradas);
-    console.log('Corretas ->' + letrasCorretas);
+    console.log('Erradas -> ' + letrasErradas);
+    console.log('Corretas -> ' + letrasCorretas);
+    console.log('Vidas -> ' + vidas);
 });
 
 
@@ -49,9 +68,8 @@ desenharTracinho(palavraSecreta); // Desenha o tracinho na tela(canvas)
 
 
 
-//#region --- Funções
-function desenharTracinho() {
-    // FIXME: mudar o nome da função, pelo amor de deus
+//#region ---> Funções
+function desenharTracinho() { // FIXME: mudar o nome da função, pelo amor de deus
     canvas.lineWidth = 6; // Espessura(densidade, grossura) da linha
     canvas.lineCap = 'round'; // Ajusta a forma de terminar a linha (cantinhos arredondados)
     canvas.lineJoin = 'round'; // Ajusta a forma quando duas linhas se encontram (cantinhos arredondados)
@@ -73,10 +91,15 @@ function eUmaLetra(intervalo) {
 }
 
 function mostrarAvisoLetraRepetida() {
-    alert('Letra já foi usada!');
+    alert('Essa letra já foi usada!');
 }
 
-function escreverLetraCorreta(indice) {
+function gameOver() {
+    alert('Game Over!');
+}
+
+// Trocar o nome desse parametro para "letraCorreta?"
+function escreverLetraCorreta(letraCorreta) {
     canvas.font = 'bold 52px Inter'; // Tipo da fonte usada (importada no HTML)
     canvas.lineWidth = 6; // Espessura(densidade, grossura) da linha
     canvas.lineCap = 'round'; // Ajusta a forma de terminar a linha (cantinhos arredondados)
@@ -84,13 +107,19 @@ function escreverLetraCorreta(indice) {
     canvas.strokeStyle = '#0A3871'; // Cor das linhas
 
     var eixo = 600/palavraSecreta.length; // Descobrir o motivo do nome????? | Espaço onde será desenhado o tracinho
-    canvas.fillText(palavraSecreta[indice], 505 + (eixo * indice), 620); // Desenha a letra na posição correta
+    canvas.fillText(palavraSecreta[letraCorreta], 505 + (eixo * letraCorreta), 620); // Desenha a letra correta acima dos tracinhos
     canvas.stroke(); // Borda
 }
 
-//! function escreverLetraIncorreta() {} ----> 42:20
+// Mesma construção de lógica de escreverLetraCorreta()
+function escreverLetraIncorreta(letraIncorreta, margemDeErro) {
+    canvas.font = 'bold 40px Inter'; // Tipo da fonte usada (importada no HTML)
+    canvas.lineWidth = 6; // Espessura(densidade, grossura) da linha
+    canvas.lineCap = 'round'; // Ajusta a forma de terminar a linha (cantinhos arredondados)
+    canvas.lineJoin = 'round'; // Ajusta a forma quando duas linhas se encontram (cantinhos arredondados)
+    canvas.strokeStyle = '#0A3871'; // Cor das linhas
 
-//! function pegandoTeclado() {}
+    canvas.fillText(palavraSecreta[letraIncorreta], 535 + (40 * (10 - margemDeErro)), 710, 40); // Desenha a letra incorreta abaixo dos tracinhos
+}
 
-
-//#endregion --- Funções
+//#endregion ---> Funções
